c84f1bd1e3682e1440bec713c10624a3ce9575a1
Micaela Patiño <micaela.patino@ucb.edu.bo>
2021-09-30 10:03:43 -0400

    Impuestos varios estados

--- src/Totalizador.test.js
+++ src/Totalizador.test.js
@@ -11,13 +11,28 @@ describe("Totalizador",() =>{
     it("deberia calcular el total para una cantidad ",()=>{
         expect(calcularTotal(3,2,"CA")).toEqual(6.495);
     });
+    it("deberia calcular el total para una cantidad ",()=>{
+        expect(calcularTotal(3,2,"NV")).toEqual(6.48);
+    });
+    it("deberia calcular el total para una cantidad ",()=>{
+        expect(calcularTotal(3,2,"TX")).toEqual(6.375);
+    });
+    it("deberia calcular el total para una cantidad ",()=>{
+        expect(calcularTotal(501,2,"NV")).toEqual(1049.6952);
+    });
 });
 
+function impuestoEstado(estado){
+    let impuestos = {"CA":0.0825,"UT":0.0665,"NV":0.08,"TX":0.0625,"AL":0.04,"":0}
+    return impuestos[estado];
+}
 function calcularTotal(cantidad,precio,estado){
     let subTotal = cantidad*precio;
-    if(estado=="CA"){
-        let impuesto = 0.0825;
-        return subTotal + subTotal*impuesto;
+    if(subTotal>1000){
+        subTotal = subTotal-subTotal*0.03
     }
+    let impuesto = impuestoEstado(estado);
+    subTotal = subTotal + subTotal*impuesto;
+    
     return subTotal;
 }
